6b5946fab99effc272bb931183faca5f011d8c02
Joseph Keelagher <KeelagherJ@gmail.com>
2024-11-21 21:11:37 +1100

    Updated Cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,28 @@ import "../App.css"
 import "../styles/Shadows.css"
 import Button from './Button'
 
-const Card = () => {
+interface Props {
+  image : string;
+  title : string;
+  description : string;
+  link : string;
+}
+
+const Card = ({image, title, description, link} : Props) => {
+  function buttonHandler(target: string) {
+    if (target) {
+      window.open(target, "_blank");
+    }
+  }
+
   return (
-    <>
-      <div className="projects__card-padding">
+    <> 
+      <div className="projects__card-padding" onClick={() => buttonHandler(link)}>
         <div className="projects__card-container drop-shadow">
           <div className="projects__card-contents-container">
-            <img src="src\assets\mid-shot.jpeg"/>
-            <h2>Example Project</h2>
-            <p>brief description</p>
+            <img src={image}/>
+            <h2>{title}</h2>
+            <p>{description}</p>
           </div>
         </div>
       </div>
